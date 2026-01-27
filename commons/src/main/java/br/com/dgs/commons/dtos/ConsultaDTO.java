e5666edd3818ad25fae3fb7ea2db91c0759efeb0
msf <marcossfelisberto@gmail.com>
2026-01-27 20:02:31 -0300

    Criando o modulo commons informações que serão uteis para serem utilizadas nos demais microsservicos.

---
+++ commons/src/main/java/br/com/dgs/commons/dtos/ConsultaDTO.java
@@ -0,0 +1,23 @@
+package br.com.dgs.commons.dtos;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record ConsultaDTO(
+        @NotNull
+        Long pacienteId,
+
+        @NotNull
+        Long medicoId,
+
+        @NotNull
+        @Future
+        LocalDateTime dataHora,
+
+        @NotBlank
+        String especialidade
+) {
+}
