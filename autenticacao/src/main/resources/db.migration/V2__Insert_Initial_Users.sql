8c9b306d5ce3e8ea60c6f9b141eff14bafb62f77
msf <marcossfelisberto@gmail.com>
2026-01-28 09:22:35 -0300

    Finalizando o pacote infrastructure.

---
+++ autenticacao/src/main/resources/db.migration/V2__Insert_Initial_Users.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_usuarios (email, senha, perfil) VALUES
+                                                   ('admin@email.com', '$2a$12$EpAmOaWrO4q1j/tyDqF3Ve8S8RLT48tI97papdnTbTfQ8K4e.sgzu', 'ADMIN'),
+                                                   ('medico@email.com', '$2a$12$EpAmOaWrO4q1j/tyDqF3Ve8S8RLT48tI97papdnTbTfQ8K4e.sgzu', 'MEDICO'),
+                                                   ('enfermeiro@email.com', '$2a$12$EpAmOaWrO4q1j/tyDqF3Ve8S8RLT48tI97papdnTbTfQ8K4e.sgzu', 'ENFERMEIRO'),
+                                                   ('paciente@email.com', '$2a$12$EpAmOaWrO4q1j/tyDqF3Ve8S8RLT48tI97papdnTbTfQ8K4e.sgzu', 'PACIENTE');
